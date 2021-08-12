c7d0732ef744873b30fda72e2013cc3c792ea0d8
Nalbert, Vinicius <rullaxfox@gmail.com>
2021-08-12 08:53:55 -0300

    criando services, modulo. adicionando material, flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/socket-io.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Message } from './interface/message';
+import socketIO from 'socket.io-client';
+import { Subject } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketIoService {
+
+  private url = 'http://localhost:5000';
+  private socket = socketIO(this.url);
+  private subMessages: Subject<Message> = new Subject<Message>(); // observable
+
+  constructor() {
+    //recebe as mensagens enviadas do servidor
+    this.socket.on('message', (m: Message) =>{
+      this.subMessages.next(m);
+    })
+  }
+
+  //envio de mensagens para servidor
+  enviarMensagem(msg: Message){
+    this.socket.emit('message', msg);  
+  }
+
+  mensagens(){
+    return this.subMessages.asObservable();
+  }
+
+}
