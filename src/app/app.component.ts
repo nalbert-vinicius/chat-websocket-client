0f066f7073bbe1a1c0b66320f690d2730b2236b1
Nalbert, Vinicius <rullaxfox@gmail.com>
2021-08-13 08:44:15 -0300

    criando função para envio de mensagem e melhorias no front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Message } from './interface/message';
+import { SocketIoService } from './socket-io.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chat-websocket-client';
+  nickName: string;
+  message: string
+
+  constructor(
+    private socketService: SocketIoService
+  ) { }
+
+  ngOnInit(){
+    this.socketService.mensagens().subscribe((msg: Message) =>{
+      console.log(msg);
+    })
+  }
+
+  enviar(){
+    this.socketService.enviarMensagem({
+      from: this.nickName,
+      message: this.message
+    });
+    this.message = '';
+  }
 }
